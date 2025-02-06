886ad804dc07ede67ac7433bac1ee35bea0906ab
Taylor King <taylorking206@u.boisestate.edu>
2025-02-06 15:13:46 -0700

    make build faster

--- src/lib/imports.ts
+++ src/lib/imports.ts
@@ -1,5 +1,7 @@
 import './styles/animate.css';
-import './styles/theme.css';
+// import './styles/theme.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/scss/colors.scss';
 import './styles/bs-overwrite.css';
 import './styles/global.css';
 import './styles/style.css';
