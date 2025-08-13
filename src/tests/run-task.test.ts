e01ef4002258ffb3b60dde1784e9a9eb1e6b0ce9
taylor <taylor.king206@u.boisestate.edu>
2025-08-12 23:37:39 -0600

    sets unit test time higher

--- src/tests/run-task.test.ts
+++ src/tests/run-task.test.ts
@@ -5,12 +5,12 @@ describe('Run a typescript file', () => {
 	it('Will run /src/tests/task-import.ts', async () => {
 		const res = await runTs('/src/tests/task-import', 'test', 'hello').unwrap();
 		expect(res).toBe('hello');
-	});
+	}, 10000);
 });
 
 describe('Run a command', () => {
 	it('Will run git status', async () => {
 		const res = await runTask('echo "hi!"').unwrap();
 		expect(res).toBe('hi!');
-	});
+	}, 10000);
 });
