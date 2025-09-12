7b307d5ddab8e0c0b6c5f3438969b995d348cd44
taylor <taylor.king206@u.boisestate.edu>
2025-09-12 11:39:14 -0600

    email microservice works!

--- src/lib/types/email.ts
+++ src/lib/types/email.ts
@@ -1,15 +1,12 @@
 export type Email = {
-	'forgot-password': {
-		link: string;
-		supportEmail: string;
-	};
-	'new-user': {
-		username: string;
-		verification: string;
-	};
-	test: {
-		service: string;
-		link: string;
-		linkText: string;
-	};
+    'forgot-password': {
+        link: string;
+        supportEmail: string;
+    };
+'test': {
+        service: string;
+        link: string;
+        linkText: string;
+    };
+
 };
