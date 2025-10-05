6524802d488158b9cc855bf81cfebb9f8ca5befa
taylor <taylor.king206@u.boisestate.edu>
2025-10-05 00:54:39 -0600

    add DoFetch to speed up timeout

--- scripts/test-integration.ts
+++ scripts/test-integration.ts
@@ -4,9 +4,20 @@ import { config } from '../src/lib/server/utils/env';
 export default async () => {
 	let tries = 0;
 
+	const doFetch = (url: string) => new Promise((res, rej) => {
+		fetch(url)
+			.then((r) => {
+				if (r.ok) res(true);
+				else rej(new Error(`Status not OK: ${r.status}`));
+			})
+			.catch((e) => rej(e));
+
+		setTimeout(() => rej(new Error('Fetch timed out')), 5000);
+	});
+
 	const run = async () => {
 		try {
-			await fetch(`http://localhost:${config.network.port}`);
+			await doFetch(`http://localhost:${config.network.port}`);
 			console.log('Server is up!');
 		} catch (error) {
 			console.error('Error occurred while checking server status:', error);
